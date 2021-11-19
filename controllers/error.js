374251f61c55f9d6df3d321003ee8cd289cd1704
rodriguezhr25 <rodriguezhr25@gmail.com>
2021-11-18 20:59:47 -0400

    cleaningErrors

--- controllers/error.js
+++ controllers/error.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 exports.get404 = (req, res, next) => {
     res.status(404).render('404' , {pageTitle: 'Page Not Found' , path: 'admin/404'} );
-=======
-exports.get404 = (req, res, next) => {
-    res.status(404).render('404' , {pageTitle: 'Page Not Found' , path: 'admin/404'} );
->>>>>>> 58dbb0b7dbfef9105531a0ceb479ce8b8c01ca4e
 };
