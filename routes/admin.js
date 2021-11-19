1a96f34689f07f678a4c57697adbc3dd3c83be8a
rodriguezhr25 <rodriguezhr25@gmail.com>
2021-11-18 21:06:09 -0400

    FIXING ERRORS

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const path = require('path');
 
 const express = require('express');
@@ -25,31 +25,4 @@ router.post('/edit-product', adminController.postEditProduct);
 // // /admin/remove-product => POST
 router.post('/delete-product', adminController.postDeleteProduct);
 module.exports = router;
-=======
-const path = require('path');
 
-const express = require('express');
-
-const adminController = require('../controllers/admin');
-
-const router = express.Router();
-
-
-// /admin/add-product => GET
- router.get('/add-product', adminController.getAddProduct );
-
-// // /admin/products => GET
-  router.get('/products', adminController.getProducts); 
-
-// // /admin/add-product => POST
- router.post('/add-product', adminController.postAddProduct);
-
- router.get('/edit-product/:productId', adminController.getEditProduct); 
-
-// // /admin/add-product => POST
-router.post('/edit-product', adminController.postEditProduct);
-
-// // /admin/remove-product => POST
-router.post('/delete-product', adminController.postDeleteProduct);
-module.exports = router;
->>>>>>> 58dbb0b7dbfef9105531a0ceb479ce8b8c01ca4e
